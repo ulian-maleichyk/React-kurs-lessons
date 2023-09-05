d81b99e8a6a05b86f7f062206990ac86af2a9202
ulian-maleichyk <121232373+ulian-maleichyk@users.noreply.github.com>
2023-09-05 16:35:10 +0300

    q

--- src/conponents/Counter.js
+++ src/conponents/Counter.js
@@ -1,4 +1,7 @@
- 
+import { Connect, connect } from "react-redux"
+import * as actions from "../actions"
+
+
  const Counter = ({counter, inc, dec, rnd}) => {
     return(
         <div className="jumbotron">
@@ -10,4 +13,30 @@
     )
  }
 
- export default Counter
+    // const mapDispatchToProps = (dispatch) => {
+    //     const {inc, dec, rnd} = bindActionCreators(actions, dispatch)
+    //     return {
+    //         // inc: () => dispatch({type: 'INC'}),
+    //         // dec: () => dispatch({type: 'DEC'}),
+    //         // rnd: () => {
+    //         //     const value = Math.floor(Math.random() * 10)
+    //         //     dispatch(rnd(value))
+    //         // },
+    //         inc,
+    //         dec,
+    //         rnd,
+    //     }
+    // }
+    
+
+    // const mapDispatchToProps = (dispatch) => {
+    //     return  bindActionCreators(actions, dispatch)
+    // }
+
+    const mapStateToProps = (state) => {
+        return {
+            counter: state.value
+        }
+    }
+// export default connect(mapStateToProps,  mapDispatchToProps )(Counter)
+ export default connect(mapStateToProps, actions)(Counter)
